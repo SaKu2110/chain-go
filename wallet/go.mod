60da62a748738a41c7997fb830753bc7c64aec07
SaKu2110 <b1018106@fun.ac.jp>
2020-02-03 07:25:55 +0900

    create wallet server model

---
+++ wallet/go.mod
@@ -0,0 +1,3 @@
+module github.com/SaKu2110/oauth_chain/wallet
+
+go 1.13
--- wallet/main.go
+++ wallet/main.go
@@ -1,8 +1,66 @@
 package main
 
 import(
+	"fmt"
+	"time"
+	"log"
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/SaKu2110/oauth_chain/wallet/config"
+	"github.com/SaKu2110/oauth_chain/wallet/controller"
+	"github.com/SaKu2110/oauth_chain/wallet/model"
 )
 
 func mian(){
+	db, err := initializeDataBase()
+	if err != nil {
+		log.Fatalf("failed initialize db. err=%s", err)
+	}
 
+	defer db.Close()
+
+	ctrl := initializeController(db)
+	router := setupRouter(ctrl)
+	err = router.Run(":9005")
+	if err != nil {
+		log.Fatalf("failed launch router. err=%s", err)
+	}
+}
+
+func initializeDataBase() (*gorm.DB, error){
+	var db *gorm.DB
+	var err error
+	var count time.Duration
+	token := config.GetConnectionToken()
+
+	count = 1
+	for {
+		if count > 5 {
+			return nil, fmt.Errorf("faild mysql connection")
+		}
+		db, err = gorm.Open("mysql", token)
+		if err == nil {
+			return db, nil
+		}
+		time.Sleep(3 * time.Second)
+
+		count++
+	}
+
+	return nil, err
+}
+
+func initializeController(db *gorm.DB) (controller.IsController){
+	return controller.IsController{DB: db}
+}
+
+func setupRouter(ctrl controller.IsController) *gin.Engine {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context){
+		c.JSON(http.StatusOK, gin.H{"message": "This is Card-Info API"})
+	})
+	router.POST("/set", ctrl.SetCardInfo)
+	return router
 }
