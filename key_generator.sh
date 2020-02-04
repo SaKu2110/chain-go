874a334ff50b238fe20412590571f3d3442114fc
SaKu2110 <b1018106@fun.ac.jp>
2020-02-04 11:06:12 +0900

    update shell

--- key_generator.sh
+++ key_generator.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-openssl genrsa -out private.key 4096
-openssl rsa -in private.key -out public.key -pubout
+openssl genrsa -out private_key.pem 4096
+openssl rsa -in private_key.pem -out public_key.pem -pubout
